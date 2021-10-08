4e1b2a4fa553933923945f6205086dad28b56aee
pdr-dev71 <joaopedrosr2699@gmail.com>
2021-10-08 15:25:53 -0300

    Project Finished

--- src/Telas/Servicos/index.js
+++ src/Telas/Servicos/index.js
@@ -7,32 +7,35 @@ import Item from './Item';
 import TelaPadrao from '../../Componentes/TelaPadrao';
 
 
-
-
 const servicos = [
         
     {
         id: 1,
-        nome: 'banho',
+        nome: 'Banho Simples',
         preco: 80.0,
-        descricao: 'deixamos seu cachorro mais limpo que conta de prefeitura'
+        descricao: 'Realização de banho sem tosagem'
     },
     {
         id: 2,
-        nome: 'vacina v8',
-        preco: 200.0,
-        descricao: 'Seu doguinho imunizado contra as doenças'
+        nome: 'Banho Completo',
+        preco: 160.0,
+        descricao: 'Realização de banho com tosa de acordo com a pelagem do animal'
     
     },
     {
         id: 3,
-        nome: 'vacina raiva',
-        preco: 80.0,
-        descricao: 'pra morder sem se preocupar'
+        nome: 'Vacina Antirrabica ',
+        preco: 50.0,
+        descricao: 'Vacina aplicada a partir dos 6 meses em Cães e Gatos'
+
+    },
+    {
+        id: 4,
+        nome: 'Vacina V8',
+        preco: 90.0,
+        descricao: 'Proteja seu doguinho contra 8 doenças'
     }
     
-    
-
 ]
 
 export default function Servicos(){
